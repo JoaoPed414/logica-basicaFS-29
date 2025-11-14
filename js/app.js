9d579f25487631dd075d0f732d889c2446437f0d
joao pedro <pedroca14tV@gmail.com>
2025-11-14 10:47:39 -0300

    Variáveis JS

---
+++ js/app.js
@@ -0,0 +1,64 @@
+
+
+/*
+var valor = 5
+console.log(valor)
+
+if (1=1) {
+    var valor = 10
+    console.log(valor)
+}
+
+
+console.log(valor)
+*/
+
+
+
+/*
+let valor2 = 10
+console.log(valor2)
+
+if (1=1){
+    let valor2 = "OK"
+    console.log(valor2)
+}
+
+console.log(valor2)
+
+*/
+
+
+
+
+
+
+
+let contador;
+
+const valor3 = "OK"
+console.log(valor3)
+
+valor3 = "Jp";
+console.log(valor3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
